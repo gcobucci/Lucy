36f07eba1b94fd223277bf1e92e44e5f437ebc71
mmassaroni <miguelmassaroni@gmail.com>
2017-09-27 00:51:40 -0300

    pipicaca

--- Lucy/Controllers/RecetasController.cs
+++ Lucy/Controllers/RecetasController.cs
@@ -54,6 +54,8 @@ namespace Lucy.Controllers
                     ViewBag.Message = "No encontramos recetas con estos filtros.";
                 }
             }
+
+            ViewBag.idUsu = idUsu;
             return View(recetas);
         }
 
