70c542cfe05bda42624999daa242c1e0b3f32738
mmassaroni <miguelmassaroni@gmail.com>
2017-10-17 01:39:32 -0300

    Listo Personas. Beta de sub menu del left menu.

--- Lucy/App_Start/BundleConfig.cs
+++ Lucy/App_Start/BundleConfig.cs
@@ -22,6 +22,7 @@ namespace Lucy
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-select.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
