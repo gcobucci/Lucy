2a14ccf379e90ae530521e85344b8cafd94e9324
mmassaroni <miguelmassaroni@gmail.com>
2017-10-19 01:46:24 -0300

    Mapa y otras cosas

---
+++ Lucy/Controllers/MapaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lucy.Controllers
+{
+    [RoutePrefix("mapa")]
+    public class MapaController : Controller
+    {
+        // GET: Mapa
+        [Route("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
