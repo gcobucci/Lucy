9cc405baa642326ab68af3ef499fb98e59b2909d
mmassaroni <miguelmassaroni@gmail.com>
2017-10-27 10:46:48 -0300

    Agregue el abm de sexos

---
+++ Backend/Controllers/SexosController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ModelCL;
+using System.IO;
+
+namespace Backend.Controllers
+{
+    [RoutePrefix("sexos")]
+    public class SexosController : Controller
+    {
+        private AgustinaEntities db = new AgustinaEntities();
+
+        [Route("listado")]
+        public ActionResult Index()
+        {
+            List<ModelCL.Sexo> sexos = db.Sexo.ToList();
+
+            return View(sexos);
+        }
+
+        [Route("crear")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("crear")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ModelCL.Sexo sexo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sexo.Add(sexo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sexo);
+        }
+
+        [Route("editar")]
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ModelCL.Sexo sexo = db.Sexo.Find(id);
+            if (sexo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sexo);
+        }
+
+        [HttpPost]
+        [Route("editar")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ModelCL.Sexo sexo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sexo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sexo);
+        }
+
+        [Route("eliminar")]
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ModelCL.Sexo sexo = db.Sexo.Find(id);
+            if (sexo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sexo);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Route("eliminar")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            ModelCL.Sexo sexo = db.Sexo.Find(id);
+
+            db.Sexo.Remove(sexo);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
